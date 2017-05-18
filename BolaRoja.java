ec18c288c373ee2f1cc7d8621e5df6cb9bde13d3
Iván <ivancalfer1@gmail.com>
2017-05-18 13:10:29 +0200

    2 commit

--- BolaRoja.java
+++ BolaRoja.java
@@ -13,8 +13,31 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Paint;
 import javafx.scene.paint.Color;
 import javafx.scene.Group;
+import javafx.animation.Timeline;
+import javafx.animation.KeyFrame;
+import javafx.util.Duration;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+
 public class BolaRoja extends Application
 {
+
+    static int dx = 1;
+    static int dy = 1;
+    
   public static void main(String[] args){
     launch(args);
   }
@@ -24,15 +47,37 @@ public class BolaRoja extends Application
   {
     Group grupo = new Group();
     
+ 
+    
     Circle bolaRoja = new Circle();
     bolaRoja.setCenterX(250);
     bolaRoja.setCenterY(250);
     bolaRoja.setRadius(20);
     Color c = Color.RED;
     bolaRoja.setFill(c);
-    
+  
     
     grupo.getChildren().add(bolaRoja);
+    Timeline tl = new Timeline();
+    tl.setCycleCount(Animation.INDEFINITE);
+    KeyFrame moveBall = new KeyFrame(Duration.seconds(.0200),
+                new EventHandler<ActionEvent>() {
+
+                    public void handle(ActionEvent event) {
+                        
+                        
+                        bolaRoja.setTranslateX(bolaRoja.getTranslateX() + dx);
+                        bolaRoja.setTranslateY(bolaRoja.getTranslateY() + dy);
+
+                        
+                            }
+                });
+    
+    
+    
+    tl.getKeyFrames().add(moveBall);
+    tl.play();
+    
        
   		
     
